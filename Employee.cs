9b3f7815ad2303bf2eae530adf29fcaa5408f343
TruongHongAnh2004 <honganhtruong46@gmail.com>
2024-09-11 01:35:19 +0700

    first commit

---
+++ Employee.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhanVienCTy
+{
+    //Đây là trừu tượng abtract class 
+    public abstract class Employee
+    {
+        //tính chất đóng gói trong CS là sd get set ( thuộc C# nâng cấp)
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Age {  get; set; }
+        public bool Gioitinh {  get; set; }
+        public string City { get; set; }
+
+        public Employee() { }
+        public Employee(string _id, string _name, int _age, bool _gt, string _ctiy)
+        {
+            this.Id = _id;
+            this.Name = _name;
+            this.Age = _age;
+            this.Gioitinh = _gt;
+            this.City = _ctiy;
+        }
+        //đa hình nhập 
+        public virtual void Nhap()
+        {
+            Console.WriteLine("Nhap thong tin Nhan Vien");
+            this.Id = Console.ReadLine();
+            this.Name = Console.ReadLine();
+            this.Age = int.Parse(Console.ReadLine());
+            this.Gioitinh = bool.Parse(Console.ReadLine());
+            this.City= Console.ReadLine();
+
+        }
+        //Đa hình để xuất nhân viên (có bao gồm trừu tượng)
+        public abstract void Output();
+    }
+    class Manager : Employee
+    {
+        public double Salary{get; set; }
+        public Manager() { }
+        public Manager(double salary)
+        {
+            Salary = salary;
+        }
+        public override void Nhap()
+        {
+            Console.Write("Luong nhan vien la: ");
+            this.Salary = double.Parse(Console.ReadLine());
+        }
+        public override void Output()
+        {
+            Console.Write($"Thong tin nhan vien: {Id} ");
+            Console.WriteLine($"Manager: {Name}, Age: {Age}, Gioi tinh: {Gioitinh}, City: {City}, Salary: {Salary}");
+        }
+    }
+
+    class Sale : Employee
+    {
+        public double Salary { get; set; }
+        public Sale() { }
+        public Sale(double salary)
+        {
+            Salary = salary;
+        }
+        public override void Nhap()
+        {
+            Console.Write("Luong nhan vien la: ");
+            this.Salary = double.Parse(Console.ReadLine());
+        }
+        public override void Output()
+        {
+            Console.Write($"Thong tin nhan vien: {Id} ");
+            Console.WriteLine($"Manager: {Name}, Age: {Age}, Gioi tinh: {Gioitinh}, City: {City}, Salary: {Salary}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics.Metrics;
+using NhanVienCTy;
+public class Program
+{
+
+    public class QuanLyEmployee
+    {
+        private List<Employee> employees = new List<Employee>();
+        //Thêm nhân viên mới vô
+
+        public void AddEmployee(Employee employee)
+        {
+            employees.Add(employee);
+        }
+
+   
+        public void Xuat()
+        {
+            foreach (var employee in employees)
+            {
+                employee.Output();
+            }
+        }
+        static void Main(string[] args)
+        {
+            QuanLyEmployee nv = new QuanLyEmployee();
+
+
+            nv.AddEmployee(new Manager(nv.Nhap());
+            nv.AddEmployee(new Sale(nv.Nhap());
+
+            Console.WriteLine("Employee List:");
+            nv.Xuat();
+        }
+    }
+}
